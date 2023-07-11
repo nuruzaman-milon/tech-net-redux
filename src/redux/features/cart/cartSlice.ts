8a370e02aa0bbe9d808a54270ef819cbc7cbc088
Njmilon <njmilon1@gmail.com>
2023-07-11 23:54:42 +0600

    add, delete & reduce item into cart

--- src/redux/features/cart/cartSlice.ts
+++ src/redux/features/cart/cartSlice.ts
@@ -17,18 +17,35 @@ export const cartSlice = createSlice({
     initialState,
     reducers:{
         addToCart: (state, action: PayloadAction<IProduct>) =>{
-
             const existingProduct = state.products.find(
-                (product) => product._id === action.payload._id
+                (product: IProduct) => product._id === action.payload._id
             );
-            if (existingProduct) {
+            if (existingProduct && existingProduct.quantity < 5) {
                 existingProduct.quantity = existingProduct.quantity! + 1;
-            } else {
+            } else if(!existingProduct){
                 state.products.push({...action.payload, quantity:1})
             }
+                
+            // else if(existingProduct.quantity < 6) {
+            // }
+        },
+
+        reduceFromCart: (state, action: PayloadAction<IProduct>) =>{
+            const existingProduct = state.products.find(
+                (product: IProduct) => product._id === action.payload._id
+            );
+            if (existingProduct && existingProduct.quantity > 1) {
+                existingProduct.quantity = existingProduct.quantity! - 1;
+            }
+        },
+
+        removeFromCart:(state, action: PayloadAction<IProduct>) =>{
+            state.products = state.products.filter(
+                (product: IProduct)=>product._id !== action.payload._id
+            )
         }
     }
 })
 
-export const {addToCart} = cartSlice.actions;
+export const {addToCart, removeFromCart, reduceFromCart} = cartSlice.actions;
 export default cartSlice.reducer;
