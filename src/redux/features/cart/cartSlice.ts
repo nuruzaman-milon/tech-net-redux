3dfa11b06e2a65a2e5ffcd64293d8aa618e13a5d
Md. Nuruzaman Milon <njmilon1@gmail.com>
2023-07-11 18:02:04 +0600

    cart redux added

---
+++ src/redux/features/cart/cartSlice.ts
@@ -0,0 +1,34 @@
+import { IProduct } from "@/types/globalTypes";
+import { createSlice } from "@reduxjs/toolkit";
+import type {PayloadAction} from "@reduxjs/toolkit";
+
+interface ICart {
+    products: IProduct[]
+}
+
+
+const initialState : ICart = {
+    products:[]
+}
+
+
+export const cartSlice = createSlice({
+    name: 'cart',
+    initialState,
+    reducers:{
+        addToCart: (state, action: PayloadAction<IProduct>) =>{
+
+            const existingProduct = state.products.find(
+                (product) => product._id === action.payload._id
+            );
+            if (existingProduct) {
+                existingProduct.quantity = existingProduct.quantity! + 1;
+            } else {
+                state.products.push({...action.payload, quantity:1})
+            }
+        }
+    }
+})
+
+export const {addToCart} = cartSlice.actions;
+export default cartSlice.reducer;
