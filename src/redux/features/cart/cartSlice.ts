f367850a6f7ce3b559e80da927cc1c906aeef2d1
Md. Nuruzaman Milon <njmilon1@gmail.com>
2023-07-12 11:25:21 +0600

    test

--- src/redux/features/cart/cartSlice.ts
+++ src/redux/features/cart/cartSlice.ts
@@ -1,22 +1,24 @@
 import { IProduct } from "@/types/globalTypes";
 import { createSlice } from "@reduxjs/toolkit";
-import type {PayloadAction} from "@reduxjs/toolkit";
+import type { PayloadAction } from "@reduxjs/toolkit";
 
 interface ICart {
-    products: IProduct[]
+    products: IProduct[];
+    total: number;
 }
 
 
-const initialState : ICart = {
-    products:[]
+const initialState: ICart = {
+    products: [],
+    total: 0
 }
 
 
 export const cartSlice = createSlice({
     name: 'cart',
     initialState,
-    reducers:{
-        addToCart: (state, action: PayloadAction<IProduct>) =>{
+    reducers: {
+        addToCart: (state, action: PayloadAction<IProduct>) => {
 
             const existingProduct = state.products.find(
                 (product) => product._id === action.payload._id
@@ -24,11 +26,11 @@ export const cartSlice = createSlice({
             if (existingProduct) {
                 existingProduct.quantity = existingProduct.quantity! + 1;
             } else {
-                state.products.push({...action.payload, quantity:1})
+                state.products.push({ ...action.payload, quantity: 1 })
             }
         }
     }
 })
 
-export const {addToCart} = cartSlice.actions;
+export const { addToCart } = cartSlice.actions;
 export default cartSlice.reducer;
